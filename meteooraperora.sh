9af2b0477b6218aeaf371a7733f2b64a2f9796a7
marcphemt <mrc.mng.mm@gmail.com>
2014-03-19 14:05:55 +0100

    Update meteooraperora.sh

--- meteooraperora.sh
+++ meteooraperora.sh
@@ -21,18 +21,22 @@
 #!/bin/bash
 
 figlet "3B METEO"
+while [ "$e" != r ]
+do
 echo "Inserisci località:" #'LOC'
 echo "(gli accenti devono essere sostituiti con ')"
 read LOC
  if [[ $LOC == *[[:space:]]* ]]; 
   then
   export LOC=`echo $LOC | tr ' ' '+'`
+  e=r
  fi;
  if [[ $LOC == *à ]];
   then
   echo "ricorda che i caratteri accentati vanno sostituiti con '"
-  exit
+  echo " "
  fi; 
+done 
 while [ 0 ]
 do
  echo "Previsioni per oggi o domani?" #'VAR'
