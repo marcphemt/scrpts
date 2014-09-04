a2cd2b11b3ae203ff8d99f87cd6e05786b34f355
marcphemt <mrc.mng.mm@gmail.com>
2014-09-04 17:06:13 +0200

    Ver 2.0 Corretto lo script/non serve più figlet

--- axelITA.sh
+++ axelITA.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+#Copyright (c) 2014 MarcPhemt
+#mrc.mng.mm@gmail.com
 #download with Axel
-figlet "AXEL"
+echo "##################"
+echo "Download with AXEL"
+echo "##################"
+echo "Ver 2.0"
 echo "Incolla l URL del sito per il download.."
 read URL
 axel -n 4 $URL
-figlet "FINISHED"
+echo "HO FINITO"
+echo "Grazie per aver usato lo script :)"
 sleep 3s
 exit
