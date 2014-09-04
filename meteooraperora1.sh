e2685a8b4879237b14e56c8b52c767a219dc61c4
marcphemt <mrc.mng.mm@gmail.com>
2014-09-04 11:48:16 +0200

    Ver 2.0 Fix degli spazi ed errori di output.

--- meteooraperora1.sh
+++ meteooraperora1.sh
@@ -35,6 +35,21 @@ read LOC
  if [[ $LOC == *à ]];
   then
   echo "ricorda che i caratteri accentati vanno sostituiti con '"
+  elif [[ $LOC == *ò ]];
+  then
+  echo "ricorda che i caratteri accentati vanno sostituiti con '"
+  elif [[ $LOC == *ù ]];
+  then
+  echo "ricorda che i caratteri accentati vanno sostituiti con '"
+  elif [[ $LOC == *è ]];
+  then
+  echo "ricorda che i caratteri accentati vanno sostituiti con '"
+  elif [[ $LOC == *é ]];
+  then
+  echo "ricorda che i caratteri accentati vanno sostituiti con '"
+  elif [[ $LOC == *ì ]];
+  then
+  echo "ricorda che i caratteri accentati vanno sostituiti con '"
  else
   e=r 
  fi;
@@ -49,7 +64,7 @@ do
 
 ######################
 
-wget http://www.3bmeteo.com/meteo/$LOC/dettagli_orari --quiet -O- 2> /dev/null -O -| tr '< > " /' ' ' | awk '/img_desc/ {print $13, $14, $15, $24, $25, $26, $33, $34, $35, $36, $37, $38 }' | sed -e 's/class=\ //g' -e 's/images\ //g' -e 's/icone\ //g' -e s/div\ //g -e 's/img_desc\ //g' -e 's/\&deg\;\C//g' -e 's/div//g' -e 's/img_desc//g' -e 's/\div//g' -e 's/images//g' -e 's/icone//g' -e 's/regione_big//g' -e 's/celsius//g' -e 's/&deg;C//g' -e 's/span//g' -e 's/class=//g' -e 's/temperature//g' -e 's/^[ \t]*//' -e 's/[-0-9]\.[0-9]$//g' -e 's/\ \ /\ /g' -e 's/piovaschi\ e\ .*$/piovaschi\ e\ schiarite\ /g'
+wget http://www.3bmeteo.com/meteo/$LOC/dettagli_orari --quiet -O- 2> /dev/null -O -| tr '< > " /' ' ' | awk '/img_desc/ {print $13, $14, $15, $24, $25, $26, $33, $34, $35, $36, $37, $38 }' | sed -e 's/class=\ //g' -e 's/images\ //g' -e 's/icone\ //g' -e s/div\ //g -e 's/img_desc\ //g' -e 's/\&deg\;\C//g' -e 's/div//g' -e 's/img_desc//g' -e 's/\div//g' -e 's/images//g' -e 's/icone//g' -e 's/regione_big//g' -e 's/celsius//g' -e 's/&deg;C//g' -e 's/span//g' -e 's/class=//g' -e 's/temperature//g' -e 's/^[ \t]*//' -e 's/[-0-9]\.[0-9]$//g' -e 's/\ \ /\ /g' -e 's/piovaschi\ e\ .*$/piovaschi\ e\ schiarite\ /g' -e 's/poche\ nubi\ .*$/poche\ nubi\ sparse\ /g'
 	echo " "
 	echo "PER TERMINARE PREMI INVIO"
 	read
@@ -63,7 +78,7 @@ wget http://www.3bmeteo.com/meteo/$LOC/dettagli_orari --quiet -O- 2> /dev/null -
 
 ######################
 
-wget http://www.3bmeteo.com/meteo/$LOC/dettagli_orari/1 --quiet -O- 2> /dev/null -O -| tr '< > " /' ' ' | awk '/img_desc/ {print $13, $14, $15, $24, $25, $26, $33, $34, $35, $36, $37, $38 }' | sed -e 's/class=\ //g' -e 's/images\ //g' -e 's/icone\ //g' -e s/div\ //g -e 's/img_desc\ //g' -e 's/\&deg\;\C//g' -e 's/div//g' -e 's/img_desc//g' -e 's/\div//g' -e 's/images//g' -e 's/icone//g' -e 's/regione_big//g' -e 's/celsius//g' -e 's/&deg;C//g' -e 's/span//g' -e 's/class=//g' -e 's/temperature//g' -e 's/^[ \t]*//' -e 's/[-0-9]\.[0-9]$//g' -e 's/\ \ /\ /g' -e 's/piovaschi\ e\ .*$/piovaschi\ e\ schiarite\ /g'
+wget http://www.3bmeteo.com/meteo/$LOC/dettagli_orari/1 --quiet -O- 2> /dev/null -O -| tr '< > " /' ' ' | awk '/img_desc/ {print $13, $14, $15, $24, $25, $26, $33, $34, $35, $36, $37, $38 }' | sed -e 's/class=\ //g' -e 's/images\ //g' -e 's/icone\ //g' -e s/div\ //g -e 's/img_desc\ //g' -e 's/\&deg\;\C//g' -e 's/div//g' -e 's/img_desc//g' -e 's/\div//g' -e 's/images//g' -e 's/icone//g' -e 's/regione_big//g' -e 's/celsius//g' -e 's/&deg;C//g' -e 's/span//g' -e 's/class=//g' -e 's/temperature//g' -e 's/^[ \t]*//' -e 's/[-0-9]\.[0-9]$//g' -e 's/\ \ /\ /g' -e 's/piovaschi\ e\ .*$/piovaschi\ e\ schiarite\ /g' -e 's/poche\ nubi\ .*$/poche\ nubi\ sparse\ /g'
 
 	echo "PER TERMINARE PREMI INVIO"
 	read
