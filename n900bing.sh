167e829bc360a8fc379c7098cee0fa3059c5aed8
marcphemt <mrc.mng.mm@gmail.com>
2014-10-02 11:31:29 +0200

    Update n900bing.sh

--- n900bing.sh
+++ n900bing.sh
@@ -20,18 +20,18 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #COLORS
-
 red="\e[0;31m"
 green="\e[0;32m"
 yell="\e[1;33m"
 blue="\e[0;34m"
 z="\e[0m"
- 
 
+function download()
+{
+echo -e "$red Downloading $z"
 # $bing is needed to form the fully qualified URL for
 # the Bing pic of the day
 bing="www.bing.com"
-
 # $xmlURL is needed to get the xml data from which
 # the relative URL for the Bing pic of the day is extracted
 #
@@ -41,81 +41,113 @@ bing="www.bing.com"
 #
 # The idx parameter determines where to start from. 0 is the current day,
 # 1 the previous day, etc.
-xmlURL="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US"
+xmlURL="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=$LOC"
 
 # $saveDir is used to set the location where Bing pics of the day
-# are stored.  $HOME holds the path of the current user's home directory
+# are stored. $HOME holds the path of the current user's home directory
 saveDir=/home/user/MyDocs/bing/
-
 # Create saveDir if it does not already exist
 #mkdir -p $saveDir
-
 # Set picture options
 # Valid options are: none,wallpaper,centered,scaled,stretched,zoom,spanned
 picOpts="zoom"
-
 # The desired Bing picture resolution to download
 # Valid options: "_1024x768" "_1280x720" "_1366x768" "_1920x1200"
 desiredPicRes="_1920x1200"
-
 # The file extension for the Bing pic
 picExt=".jpg"
-
 # Extract the relative URL of the Bing pic of the day from
 # the XML data retrieved from xmlURL, form the fully qualified
 # URL for the pic of the day, and store it in $picURL
-
 # Form the URL for the desired pic resolution
 desiredPicURL=$bing$(echo $(curl -s $xmlURL) | grep -oP "<urlBase>(.*)</urlBase>" | cut -d ">" -f 2 | cut -d "<" -f 1)$desiredPicRes$picExt
-
 # Form the URL for the default pic resolution
 defaultPicURL=$bing$(echo $(curl -s $xmlURL) | grep -oP "<url>(.*)</url>" | cut -d ">" -f 2 | cut -d "<" -f 1)
-
 # $picName contains the filename of the Bing pic of the day
-
 # Attempt to download the desired image resolution. If it doesn't
 # exist then download the default image resolution
+if wget --quiet --spider "$desiredPicURL"
+then
+# Set picName to the desired picName
+picName=${desiredPicURL##*/}
+# Download the Bing pic of the day at desired resolution
+curl -s -o $saveDir$picName $desiredPicURL
+#mv ~/MyDocs/bing/$picName ~/MyDocs/bing/$TODAY_bing.jpg
+else
+# Set picName to the default picName
+picName=${defaultPicURL##*/}
+# Download the Bing pic of the day at default resolution
+curl -s -o $saveDir$picName $defaultPicURL
+fi;
+
+wa
+}
+
+function location()
+{
+
+echo "Choose location:"
+echo "Japan (1), USA (2), England (3), German (4), New Zeland (5), China (6)"
+read A
+ if [ $A == 1 ]; then
+ LOC='ja-JP'
+ elif [ $A == 2 ]; then
+ LOC='en-US'
+ elif [ $A == 3 ]; then
+ LOC='en-UK'
+ elif [ $A == 4 ]; then
+ LOC='de-DE'
+ elif [ $A == 5 ]; then
+ LOC='en-NZ'
+ elif [ $A == 6 ]; then
+ LOC='zh-CN'
+ fi
+ 
+ download
+ 
+ }
+ 
+function begin()
+{
+ 
+
 echo "===================="
 echo "== BING Wallpaper =="
 echo "===================="
-
 echo "*******************************************************"
 echo -e "* This script will download and set$yell AUTOMATICALLY$z the *"
-echo "* BING daily photo in your machine.                   *"
+echo "* BING daily photo in your machine. *"
 echo -e "* To set the download lacation$green open this script with$z *"
 echo -e "*$green your notepad$z (like leafpad, medit, etc) and go to *"
 echo -e "*$yell OPTIONS$z, then set$red PICTURES_DIR=~/$z of your choise. *"
-echo -e "*$blue This script is set up for N900 ONLY $z                *"
+echo -e "*$blue This script is set up for N900 ONLY $z *"
 echo "*******************************************************"
 echo "To continue press ENTER"
 read
-if [ ! -d "$saveDir" ]; then
-mkdir $saveDir
-fi
 
-if wget --quiet --spider "$desiredPicURL"
-then
+location
+}
 
-    # Set picName to the desired picName
-    picName=${desiredPicURL##*/}
-    # Download the Bing pic of the day at desired resolution
-    curl -s -o $saveDir$picName $desiredPicURL
-	#mv ~/MyDocs/bing/$picName ~/MyDocs/bing/$TODAY_bing.jpg
-else
-    # Set picName to the default picName
-    picName=${defaultPicURL##*/}
-    # Download the Bing pic of the day at default resolution
-    curl -s -o $saveDir$picName $defaultPicURL
-fi
 
+function wa()
+{
 
 /usr/bin/dbus-send --print-reply --dest=com.nokia.image_viewer /com/nokia/image_viewer com.nokia.image_viewer.mime_open string:file:///home/user/MyDocs/bing/$picName
-echo "If you like the new photo press ENTER otherwise just close x-term!"
-read
+echo "If you like the image just press enter."
+echo "if you want to see other images type r."
+echo "if you don't like the image just close the terminal!"
+read VAR
+if [ "$VAR" = "r" ]; then
+rm /home/user/MyDocs/bing/$picName
+location
+else
+echo "Setting image as wallpaper"
+gconftool -s --type string /apps/osso/hildon-desktop/views/1/bg-image /home/user/MyDocs/bing/$picName
+gconftool -s --type string /apps/osso/hildon-desktop/views/2/bg-image /home/user/MyDocs/bing/$picName
+gconftool -s --type string /apps/osso/hildon-desktop/views/3/bg-image /home/user/MyDocs/bing/$picName
+gconftool -s --type string /apps/osso/hildon-desktop/views/4/bg-image /home/user/MyDocs/bing/$picName
+exit 
+fi;
+}
 
-  gconftool -s --type string /apps/osso/hildon-desktop/views/1/bg-image /home/user/MyDocs/bing/$picName
-	gconftool -s --type string /apps/osso/hildon-desktop/views/2/bg-image /home/user/MyDocs/bing/$picName
-	gconftool -s --type string /apps/osso/hildon-desktop/views/3/bg-image /home/user/MyDocs/bing/$picName
-	gconftool -s --type string /apps/osso/hildon-desktop/views/4/bg-image /home/user/MyDocs/bing/$picName
-
-exit
+begin
